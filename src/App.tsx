599d2001e175bab947752c9d337b9b1a6a861bc3
hiro <>
2021-06-11 19:42:37 +0900

    create Sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Sidebar } from './Sidebar';
 
 function App() {
   return (
     //BEM
     <div className="app">
       <h1>Hello this is Study with app!! Just like a study twitter!</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+      
+      {/* Widgets */}
+      
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* App Icon */}
+      <TwitterIcon />
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button => Post */}
+    </div>
+  );
+};
