dd26d672de4c2bc58e7e92fb423f8de60f01fa1f
hiro <>
2021-06-12 03:05:29 +0900

    add more icons

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import { SidebarOption } from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import ExploreIcon from '@material-ui/icons/Explore';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentitiyIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
 
 function App() {
   return (
@@ -17,7 +23,12 @@ function App() {
       
       <SidebarOption Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={ExploreIcon} text="Explore" />
-      <SidebarOption Icon={NotificationsIcon} text="Notifications" /> 
+      <SidebarOption Icon={NotificationsIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentitiyIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" /> 
 
       {/* Feed */}
       
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -9,4 +9,3 @@ export const SidebarOption = ({text, Icon}) => {
     </div>
   )
 }
-  
