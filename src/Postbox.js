e4c78c2a5c12c222a5a74a9565382dfb492ff1d1
hiro <>
2021-06-12 15:57:34 +0900

    add styles for postBox

--- src/Postbox.js
+++ src/Postbox.js
@@ -1,14 +1,16 @@
 import { Avatar, Button } from '@material-ui/core'
 import React from 'react'
+import "./PostBox.css";
 
 export const Postbox = () => {
   return (
     <div className="postBox">
-      <form action="">
-      <div className="postBox_input">
-        <Avatar src="https://pbs.twimg.com/media/Ei3ftPrWkAASkc_.jpg" />
-        <input type="text" placeholder="What's happening?"/>
-      </div>
+      <form>
+        <div className="postBox_input">
+          <Avatar src="https://pbs.twimg.com/media/Ei3ftPrWkAASkc_.jpg" />
+          <input type="text" placeholder="What's happening?"/>
+          {/* <input type="text" placeholder="Enter image URL" /> */}
+        </div>
         <Button>Post</Button>
       </form>
     </div>
