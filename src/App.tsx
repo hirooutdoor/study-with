e2b9b80a7dcfb385704321d2826cb5523aeaaf4b
hiro <>
2021-06-13 04:46:36 +0900

    add widgets

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import { Sidebar } from './Sidebar';
 import Feed from './Feed';
-import Widget from "./Widget";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      
       <Sidebar />
-      {/* Feed */}
       <Feed />
-      {/* Widgets */}
-      <Widget />
-
+      <Widgets />
     </div>
   );
 }
